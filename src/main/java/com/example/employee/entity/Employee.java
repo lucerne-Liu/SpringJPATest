b8dfc5b302ba1ea104cbeb57bf2979f6cf69d52e
Lucerne <lucerne_liu@foxmail.com>
2018-04-18 14:53:45 +0800

    feat:reclone the project and copy the content from https://github.com/lucerne-Liu/SpringDataJPATest.git

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,87 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+    @Id
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column
+    private String gender;
+
+    @Column(nullable = false)
+    private Integer companyId;
+
+    @Column(nullable = false)
+    private Integer salary;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, Integer age, String gender, Integer companyId, Integer salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
+
--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,16 +14,36 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //以下所有的*都代表变量
 
     //1.查询名字是*的第一个employee
+    Employee findTopByName(String name);
+    //方法二
+    Employee queryFirstByName(String name);
+    //方法三
+    Employee findFirstByName(String name);
 
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
+    Employee findFirstByNameContainingAndSalaryGreaterThan(String nameContains,Integer salary);
+    //方法二：测试自定义SQL查询
+    @Query(value = "select * from Employee t where t.name like %?1% and t.salary > ?2 limit 1", nativeQuery = true)
+    Employee findUserByNameCharacterAndSalary(String nameContains,Integer salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
+    @Query(value = "SELECT * FROM Employee t WHERE t.companyId =:companyId ORDER BY salary DESC limit 1", nativeQuery = true)
+    Employee findMaxSalaryByCompanyId(@Param("companyId") Integer companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
+    Page<Employee> findAll(Pageable pageable);
 
     //5.查找**的所在的公司的公司名称
+    @Query("select b.companyName from Employee a, Company b where a.companyId = b.id and a.name = ?1")
+    String findCompanyNameByEmployeeName(String name);
 
     //6.将*的名字改成*,输出这次修改影响的行数
+    @Modifying
+    @Query("update Employee u set u.name = ?1 where u.name = ?2")
+    int modifyByIName(String newName, String name);
 
     //7.删除姓名是*的employee
+    @Modifying
+    @Query("delete from Employee where name = ?1")
+    void deleteByName(String name);
 }
